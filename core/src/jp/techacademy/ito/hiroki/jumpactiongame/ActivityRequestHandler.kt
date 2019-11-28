fb2dcec9e584341e883f8b5492c4efb0f53785d6
Hitoki <diavolos1230@gmail.com>
2019-11-28 19:51:50 +0900

    Chapter13ストップ

---
+++ core/src/jp/techacademy/ito/hiroki/jumpactiongame/ActivityRequestHandler.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.ito.hiroki.jumpactiongame
+
+interface ActivityRequestHandler {
+    fun showAds(show: Boolean)
+}
--- core/src/jp/techacademy/ito/hiroki/jumpactiongame/ResultScreen.kt
+++ core/src/jp/techacademy/ito/hiroki/jumpactiongame/ResultScreen.kt
@@ -23,6 +23,9 @@ class ResultScreen(private val mGame: JumpActionGame, private val mScore: Int) :
     private var mFont: BitmapFont
 
     init {
+        if (mGame.mRequestHandler != null) {
+            mGame.mRequestHandler.showAds(true)
+        }
 
         // 背景の準備
         val bgTexture = Texture("resultback.png")
@@ -55,6 +58,9 @@ class ResultScreen(private val mGame: JumpActionGame, private val mScore: Int) :
         mGame.batch.end()
 
         if (Gdx.input.justTouched()) {
+            if (mGame.mRequestHandler != null) {
+                mGame.mRequestHandler.showAds(false)
+            }
             mGame.screen = GameScreen(mGame)
         }
     }
